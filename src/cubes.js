3abd62eceae204321962378a3e07352662cbb60a
Fraser Sharp <fraserrsharp@gmail.com>
2015-06-09 17:28:09 +0100

    Added noise.js library, added basic dynamic block generation, and created method to create 8x8 cube of blocks

--- src/cubes.js
+++ src/cubes.js
@@ -16,6 +16,7 @@ var VIEW_ANGLE = 45,
   ASPECT = WIDTH/HEIGHT,
   NEAR = 0.1,
   FAR = 10000;
+
 var $container;
 
 var renderer;
@@ -23,8 +24,18 @@ var camera;
 var scene;
 var controls;
 
+var terrain;
+var terrainSize = new THREE.Vector3(8,8,8);
+
 init();
 renderLoop();
+
+function block(type) {
+	this.type = type;
+	var geometry = new THREE.BoxGeometry(1,1,1);
+	var material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
+	this.mesh = new THREE.Mesh( geometry, material );
+}
 
 function init() {
 	prepareWebGL();
@@ -50,17 +61,48 @@ function generateWorld() {
 	addCamera();
 	addLighting();
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+	// var cube = new THREE.Mesh( geometry, material );
+	// scene.add( cube );
+
+	terrain = [];
+
+	for (var i=0; i < terrainSize.x; i++)
+	{
+		terrain[i] = [];
+		for (var j=0; j < terrainSize.y; j++)
+		{
+			terrain[i][j] = [];
+			for (var k=0; k < terrainSize.z; k++)
+			{
+				terrain[i][j][k] = new block("dirt");
+			}
+		}
+	}
+
+	for (var i=0; i < terrainSize.x; i++)
+	{
+		for (var j=0; j < terrainSize.y; j++)
+		{
+			for (var k=0; k < terrainSize.z; k++)
+			{
+				if (terrain[i][j][k].type == "dirt")
+				{
+					var cube = new block("dirt");
+					scene.add( cube.mesh );
+					cube.mesh.position.x = i;
+					cube.mesh.position.y = j;
+					cube.mesh.position.z = k;
+				}
+			}
+		}
+	}
 }
 
 function addCamera() {
 	scene.add(camera);
 
 	camera.position.set(6,4,8);
-	camera.lookAt(new THREE.Vector3(0,0,0));
+	camera.lookAt(new THREE.Vector3(terrainSize.x/2,terrainSize.y/2,terrainSize.z/2));
 	
 	controls = new THREE.OrbitControls( camera );
 	controls.damping = 0.2;
@@ -85,4 +127,5 @@ function updateWorld() {
 
 function draw() {
 	renderer.render(scene, camera);
+
 }
